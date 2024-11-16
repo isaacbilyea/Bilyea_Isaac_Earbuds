7c074164fa5cb41baeacb54ed0fc594cedd26d81
Isaac Bilyea <isaacbilyea@gmail.com>
2024-11-16 14:41:10 -0500

    Add video and hero
    Add video and hero

--- js/main.js
+++ js/main.js
@@ -34,6 +34,23 @@
 
 })();
 
+//Plyr - Video Player
+(() => {
+
+const player = new Plyr('video', {
+  settings: [
+      'play-large',
+      'play',    
+      'progress', 
+      'current-time',
+      'mute',     
+      'volume',
+      'fullscreen' 
+  ]
+  }); //Sets controls to remove defaults to get rid of settings
+
+})();
+
 //Earbuds Scrub
 (() => {
 
